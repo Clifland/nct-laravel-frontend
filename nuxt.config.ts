db71e25bfd631747602852453aa1a1c1c3c7582a
Cliford Pereira <cliford.pereira@gmail.com>
2026-06-25 15:27:25 +0530

    feat: add package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', 'nuxt-crud-table'],
   css: ['~/assets/css/main.css']
 })
